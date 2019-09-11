6972029c142a0426d165293f59e98ecdb7c11664
Matteo Monchiero <matteo@streamlit.io>
2019-09-11 15:00:11 -0700

    more fixes and cleanup

--- streamlit_demo/self_driving/app.py
+++ streamlit_demo/self_driving/app.py
@@ -1,44 +1,71 @@
 # -*- coding: utf-8 -*-
+# Copyright 2018-2019 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import streamlit as st
 import altair as alt
 import pandas as pd
 import numpy as np
 import os
 import urllib
-import cv2
-import time
-import tempfile
-import concurrent.futures
 import hashlib
 import cv2
+from collections import OrderedDict
+
+#################################################################
+# Self-driving car Demo                                         #
+#################################################################
+# Available at https://github.com/streamlit/demo-self-driving   #
+#                                                               #
+# Demo of Yolov3 Real-Time Object Detection with Streamlit on   #                                                            #
+# the Udacity Self-driving-car dataset.                         #
+#                                                               #
+# Yolo: https://pjreddie.com/darknet/yolo/                      #
+# Udacity dataset: https://github.com/udacity/self-driving-car  #
+# Streamlit: https://github.com/streamlit/streamlit             #
+#                                                               #
+# Description:                                                  #
+# The demo runs the YOLO object detection on images from the    #
+# Udacity dataset. The demo allows the user to select images    #
+# it shows a reference 'ground image' and an image with the     #
+# detected objects.                                             #
+# The demo generates a Streamlit app with the following main    #
+# characteristics:                                              #
+# - Needed data files are downloaded from the internet.         #
+# - A sidebar allows a user to selects important parameters.    #
+# - A ground image with boxes representing the objects detected #
+# by a uman is shown.                                           #
+# - The same image with boxes computed by running the YOLO      #
+# model is shown for easy comparison.                           #
+# - Some parameters of the YOLO model can be tuned by the use   #
+# of silders.                                                   #
+#################################################################
+
+
+#################
+#   Constants   #
+#################
 
 DATA_URL_ROOT = 'https://streamlit-self-driving.s3-us-west-2.amazonaws.com/'
 LABELS_FILENAME = os.path.join(DATA_URL_ROOT, 'labels.csv.gz')
-LABEL_COLORS = {
-    'car': [255, 0, 0],
-    'pedestrian': [0, 255, 0],
-    'truck': [0, 0, 255],
-    'trafficLight': [255, 255, 0],
-    'biker': [255, 0, 255],
-}
-
-WEIGHTS_FILE = 'yolov3.weights'
-
-# load the COCO class labels our YOLO model was trained on
 LABELS_PATH = "coco.names"
 WEIGHTS_PATH = "yolov3.weights"
-my_path = os.path.abspath(os.path.dirname(__file__))
 CONFIG_PATH = 'yolov3.cfg'
 
-# sorted?
-EXTERNAL_FILES = {
+EXTERNAL_FILES = OrderedDict({
     'yolov3.weights': {
         'md5': 'c84e5b99d0e52cd466ae710cadf6d84c',
         'url': 'https://pjreddie.com/media/files/yolov3.weights'
-    },
-    'yolov3-tiny.weights': {
-        'md5': '3bcd6b390912c18924b46b26a9e7ff53',
-        'url': 'https://pjreddie.com/media/files/yolov3-tiny.weights'
     },
     'coco.names': {
         'md5': '8fc50561361f8bcf96b0177086e7616c',
@@ -48,24 +75,40 @@ EXTERNAL_FILES = {
         'md5': 'b969a43a848bbf26901643b833cfb96c',
         'url': 'https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov3.cfg'
     }
+})
+
+LABEL_COLORS = {
+    'car': [255, 0, 0],
+    'pedestrian': [0, 255, 0],
+    'truck': [0, 0, 255],
+    'trafficLight': [255, 255, 0],
+    'biker': [255, 0, 255],
 }
 
-def file_downloaded(filename):
-    if not filename in EXTERNAL_FILES:
-        raise Exception('Unknown file: {}'.format(filename))
-    if not os.path.exists(filename):
+
+#################
+
+# Check if file has been downloaded.
+def file_downloaded(file_path):
+    if file_path not in EXTERNAL_FILES:
+        raise Exception('Unknown file: %s' % file_path)
+    if not os.path.exists(file_path):
         return False
-    with open(filename, 'rb') as f:
+    with open(file_path, 'rb') as f:
         m = hashlib.md5()
         m.update(f.read())
-        if str(m.hexdigest()) != EXTERNAL_FILES[filename]['md5']:
+        if str(m.hexdigest()) != EXTERNAL_FILES[file_path]['md5']:
             return False
     return True
 
 
-def download_file(filename):
+# Download a file. Report progress using st.progress that draws a progress bar on the
+# Streamlit UI.
+def download_file(file_path):
+    if file_path not in EXTERNAL_FILES:
+        raise Exception('Unknown file: %s' % file_path)
     try:
-        DOWNLOAD_MESSAGE = 'Downloading {}...'.format(filename)
+        DOWNLOAD_MESSAGE = 'Downloading %s...' % file_path
         title = st.markdown("""
             # Self Driving Car Demo
 
@@ -73,8 +116,8 @@ def download_file(filename):
             """)
         weights_warning = st.warning(DOWNLOAD_MESSAGE)
         progress_bar = st.progress(0)
-        with open(filename, 'wb') as fp:
-            with urllib.request.urlopen(EXTERNAL_FILES[filename]['url']) as response:
+        with open(file_path, 'wb') as fp:
+            with urllib.request.urlopen(EXTERNAL_FILES[file_path]['url']) as response:
                 length = int(response.info()['Content-Length'])
                 counter = 0.0
                 while True:
@@ -100,8 +143,6 @@ def download_file(filename):
 @st.cache
 def load_metadata(url):
     metadata = pd.read_csv(url)
-    # one_hot_encoded = pd.get_dummies(metadata[['frame', 'label']], columns=['label'])
-    # summary = one_hot_encoded.groupby(['frame']).sum()
     return metadata
 
 # An amazing property of st.cache'ed functions is that you can pipe them into
@@ -114,7 +155,8 @@ def create_summary(metadata):
     summary = one_hot_encoded.groupby(['frame']).sum()
     return summary
 
-@st.cache# (show_spinner=False)
+# This function loads an image from Streamlit public repo on S3.
+@st.cache  #(show_spinner=False)
 def load_image(url):
     with urllib.request.urlopen(url) as response:
         image = np.asarray(bytearray(response.read()), dtype="uint8")
@@ -122,6 +164,7 @@ def load_image(url):
     image = image[:,:,[2,1,0]] # BGR -> RGB
     return image
 
+# Add boxes to an image
 def add_boxes(image, boxes):
     image = image.astype(np.float64)
     for _, (xmin, ymin, xmax, ymax, label) in boxes.iterrows():
@@ -129,66 +172,11 @@ def add_boxes(image, boxes):
         image[ymin:ymax,xmin:xmax,:] /= 2
     return image.astype(np.uint8)
 
+
 @st.cache
 def get_selected_frames(summary, label, min_elts, max_elts):
     return summary[np.logical_and(summary[label] >= min_elts, summary[label] <= max_elts)].index
 
-def main():
-    for filename, info in EXTERNAL_FILES.items():
-        if not file_downloaded(filename):
-            download_file(filename)
-
-    metadata = load_metadata(LABELS_FILENAME)
-    summary = create_summary(metadata)
-
-    st.sidebar.title('Frame')
-    label = st.sidebar.selectbox('label', summary.columns)
-    min_elts, max_elts = st.sidebar.slider(label, 0, 25, [10, 20])
-
-    selected_frames = get_selected_frames(summary, label, min_elts, max_elts)
-    if len(selected_frames) < 1:
-        st.error('No frames fit the criteria. 😳 Please select different label or number. ✌️')
-        return
-
-    frames = metadata.frame.unique()
-    objects_per_frame = summary.loc[selected_frames, label].reset_index(drop=True).reset_index()
-
-    selected_frame_index = st.sidebar.slider(label + ' frame', 0, len(selected_frames) - 1, 0)
-    selected_frame = selected_frames[selected_frame_index]
-    image_url = os.path.join(DATA_URL_ROOT, selected_frame)
-    image = load_image(image_url)
-
-    chart = alt.Chart(objects_per_frame, height=120).mark_area().encode(
-        alt.X('index:Q', scale=alt.Scale(nice=False)),
-        alt.Y('%s:Q' % label))
-    selected_frame_df = pd.DataFrame({'selected_frame': [selected_frame_index]})
-    vline = alt.Chart(selected_frame_df).mark_rule(color='red').encode(
-        alt.X('selected_frame:Q',axis=None)
-    )
-    st.sidebar.altair_chart(alt.layer(chart, vline))
-    boxes = metadata[metadata.frame == selected_frame].drop(columns=['frame'])
-
-    st.write("### Ground Truth `%i`/`%i` : `%s`" % (selected_frame_index, len(selected_frames), selected_frame))
-
-    image_with_boxes = add_boxes(image, boxes)
-    st.image(image_with_boxes, use_column_width=True)
-
-    st.sidebar.markdown('----\n # Model')
-    st.sidebar.markdown('')
-    if st.sidebar.checkbox('Run Yolo Detection', False):
-        confidence_threshold = st.sidebar.slider('confidence_threshold', 0.0, 1.0, 0.5, 0.01)
-        overlap_threshold = st.sidebar.slider('overlap threshold', 0.0, 1.0, 0.3, 0.01)
-
-        yolo_boxes = yolo_v3(image,
-            overlap_threshold=overlap_threshold,
-            confidence_threshold=confidence_threshold,
-            weights_path=WEIGHTS_FILE)
-        image3 = add_boxes(image, yolo_boxes)
-        st.write('### YOLO Detection (overlap `%3.1f`) (confidence `%3.1f`)' % \
-            (overlap_threshold, confidence_threshold))
-        st.image(image3, use_column_width=True)
-    else:
-        st.warning('Click _Run Yolo Detection_ on the left to compare with ground truth.')
 
 @st.cache
 def get_labels_and_colors(labels_path):
@@ -201,7 +189,8 @@ def get_labels_and_colors(labels_path):
 
     return labels, colors
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
+
+# Load our YOLO object detector trained on COCO dataset (80 classes).
 @st.cache(ignore_hash=True)
 def load_network(config_path, weights_path):
     net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
@@ -211,7 +200,13 @@ def load_network(config_path, weights_path):
     output_layer_names = [output_layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
     return net, output_layer_names
 
-def yolo_v3(image, confidence_threshold=0.5, overlap_threshold=0.3, weights_path=WEIGHTS_PATH, config_path=CONFIG_PATH):
+
+# Run the YOLO network to detect objects.
+def yolo_v3(image,
+            confidence_threshold=0.5,
+            overlap_threshold=0.3,
+            weights_path=WEIGHTS_PATH,
+            config_path=CONFIG_PATH):
     # Load the network. Because this is cached it will only happen once.
     net, output_layer_names = load_network(config_path, weights_path)
 
@@ -356,7 +351,7 @@ def yolo_v3(image, confidence_threshold=0.5, overlap_threshold=0.3, weights_path
         # loop over the indexes we are keeping
         for i in idxs.flatten():
             label = new_labels[class_IDs[i]]
-            if label == None:
+            if label is None:
                 continue
 
                 # extract the bounding box coordinates
@@ -377,3 +372,90 @@ def yolo_v3(image, confidence_threshold=0.5, overlap_threshold=0.3, weights_path
         'labels': labels
     })
 
+
+########
+# Main #
+########
+def main():
+    # Download data files
+    for file, info in EXTERNAL_FILES.items():
+        if not file_downloaded(file):
+            download_file(file)
+
+    # Load metadata from S3
+    metadata = load_metadata(LABELS_FILENAME)
+    # Create a summary our of the metadata
+    summary = create_summary(metadata)
+
+    # Draw the sidebar
+    st.sidebar.title('Frame')
+    # Draw a picker for the labels
+    label = st.sidebar.selectbox('label', summary.columns)
+    # Draw a slider
+    min_elts, max_elts = st.sidebar.slider(label, 0, 25, [10, 20])
+
+    # Select frames based on the selection in the sidebar
+    selected_frames = get_selected_frames(summary, label, min_elts, max_elts)
+    if len(selected_frames) < 1:
+        st.error('No frames fit the criteria. 😳 Please select different label or number. ✌️')
+        return
+
+    frames = metadata.frame.unique()
+    objects_per_frame = summary.loc[selected_frames, label].reset_index(drop=True).reset_index()
+
+    # Select a frame out of the selecte frames.
+    selected_frame_index = st.sidebar.slider(label + ' frame', 0, len(selected_frames) - 1, 0)
+    selected_frame = selected_frames[selected_frame_index]
+    # Compose the image url for the frame
+    image_url = os.path.join(DATA_URL_ROOT, selected_frame)
+    # load the image
+    image = load_image(image_url)
+
+    # Add boxes for objects on the image as an Altair layer. These are the boxes for the ground image.
+    chart = alt.Chart(objects_per_frame, height=120).mark_area().encode(
+        alt.X('index:Q', scale=alt.Scale(nice=False)),
+        alt.Y('%s:Q' % label))
+    selected_frame_df = pd.DataFrame({'selected_frame': [selected_frame_index]})
+    vline = alt.Chart(selected_frame_df).mark_rule(color='red').encode(
+        alt.X('selected_frame:Q',axis=None)
+    )
+    st.sidebar.altair_chart(alt.layer(chart, vline))
+    boxes = metadata[metadata.frame == selected_frame].drop(columns=['frame'])
+
+    # Create an header for the ground image
+    st.write("### Ground Truth `%i`/`%i` : `%s`" % (selected_frame_index, len(selected_frames), selected_frame))
+
+    # Draw the ground image with the boxes that show the objects
+    image_with_boxes = add_boxes(image, boxes)
+    st.image(image_with_boxes, use_column_width=True)
+
+    # Add a section in the sidebar for the model.
+    st.sidebar.markdown('----\n # Model')
+    # This is an empty line
+    st.sidebar.markdown('')
+
+    # Draw a checkbox and depending on the user's choice either run the model ot show a warning.
+    if st.sidebar.checkbox('Run Yolo Detection', False):
+        # This block runs the YOLO model
+        # Add two sliders in the sidebar for confidence threshold and overlap threshold
+        # These are parameters of the models. Whe the user changes these sliders, the model re-runs.
+        confidence_threshold = st.sidebar.slider('confidence_threshold', 0.0, 1.0, 0.5, 0.01)
+        overlap_threshold = st.sidebar.slider('overlap threshold', 0.0, 1.0, 0.3, 0.01)
+
+        # Get the boxes for the objects detected by YOLO by running the YOLO model.
+        yolo_boxes = yolo_v3(image,
+            overlap_threshold=overlap_threshold,
+            confidence_threshold=confidence_threshold)
+        # Add the boxes to the image.
+        image_yolo = add_boxes(image, yolo_boxes)
+        # Add an header
+        st.write('### YOLO Detection (overlap `%3.1f`) (confidence `%3.1f`)' % \
+            (overlap_threshold, confidence_threshold))
+        # Draw the image with the boxes computed by YOLO. This image has the detected objects.
+        st.image(image_yolo, use_column_width=True)
+    else:
+        st.warning('Click _Run Yolo Detection_ on the left to compare with ground truth.')
+
+
+if __name__ == '__main__':
+    main()
